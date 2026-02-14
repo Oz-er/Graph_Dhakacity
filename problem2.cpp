d5005942300b33129dabcca73b5856f5edaaae80
Omer <omerwork3388@gmail.com>
2026-02-14 22:39:58 +0600

    problem 2,added a new struct edge,updated the graph from vector of pairs to vector of edges(will do the same with prob1),added getCost function to calculate the transit costs,updated the djikstra to compare the minimum cost instead ofthe min distance

---
+++ problem2.cpp
@@ -0,0 +1,337 @@
+#include<bits/stdc++.h>
+#include<cmath>
+using namespace std;
+
+
+double INF = 1e18;
+
+struct Node
+{
+    double lon,lat;
+
+    bool operator<(const Node& other) const {
+        if(lon!=other.lon)return lon < other.lon;
+        return lat<other.lat;
+    }
+};
+
+
+
+
+struct Edge{
+    int to;
+    double dist;
+    int mode;
+};
+
+
+
+
+
+
+
+map<Node,int>nodeToID;
+vector<Node>idToNode;
+
+
+
+vector<vector<Edge>> graph;
+
+
+int getID(Node p){
+    
+    if(nodeToID.count(p)){
+        return nodeToID[p];
+    }
+
+    int id = nodeToID.size();
+    nodeToID[p] = id;
+    idToNode.push_back(p);
+
+    graph.resize(id+1);
+    return id;
+}
+
+
+
+
+
+
+double haversine(Node p1, Node p2){
+
+    const double R = 6371.0;
+    const double PI = 3.14159265358979323846;
+
+
+    double lat1InRad = ((p1.lat)*PI)/180.0;
+    double lat2InRad = ((p2.lat)*PI)/180.0;
+
+    double latDiff = p2.lat - p1.lat;  
+    double lonDiff = p2.lon - p1.lon;
+    
+    double latDiffInRad = (latDiff*PI)/180.0;
+    double lonDiffInRad = (lonDiff*PI)/180.0;
+
+
+    double a = sin(latDiffInRad/2)*sin(latDiffInRad/2)+
+               cos(lat1InRad)*cos(lat2InRad)* 
+               sin(lonDiffInRad/2)*sin(lonDiffInRad/2);
+
+    double c = 2*atan2(sqrt(a),sqrt(1-a));
+
+    return R*c;
+
+}
+
+
+
+double getCost(double distance,int mode){
+
+    if(mode==1){
+        return distance*20.0;
+    }
+
+    if(mode==2){
+        return distance*5.0;
+    }
+
+    return 0.0;
+
+}
+
+
+
+
+
+
+vector<int> dijkstra(int startNode, int endNode){
+
+    int n = graph.size();
+
+    
+    
+    vector<double>minCost(n,INF);
+    minCost[startNode]=0.0;
+    vector<int>parent(n,-1);
+
+
+
+    //priority_queue<1,2,3>
+    //1 = what the queue is storing (pair in this case),
+    //    pairs are sorted by the first element
+    //2 = container, using vector to hold the pairs in memory
+    //3 = comperator, by default its max heap
+    //    using greater<> reverses the behaviour
+
+
+
+    priority_queue<pair<double,int>,vector<pair<double,int>>,greater<pair<double,int>>> pq;
+
+    
+    pq.push({0.0, startNode});
+
+
+    while(!pq.empty()){
+        
+        double currentCost = pq.top().first;
+        int id = pq.top().second;
+        pq.pop();
+
+
+
+        if(currentCost >minCost[id]){
+            continue;
+        }
+        if(id==endNode){
+            break;
+        }
+
+
+
+
+        for(auto edge : graph[id]){
+        
+            int neighborID = edge.to;
+            double edgeToNeighbor = edge.dist;
+            int mode = edge.mode;
+
+         
+            double edgeCost = getCost(edgeToNeighbor,mode);
+    
+
+            if(minCost[id] + edgeToNeighbor < minCost[neighborID]){
+                minCost[neighborID]=minCost[id]+edgeToNeighbor;
+                parent[neighborID]=id;
+                pq.push({minCost[neighborID],neighborID});
+            }
+        
+        
+        
+        }
+    }
+
+
+    vector<int>path;
+    if(minCost[endNode] == INF){
+        return path;
+    }
+
+    for(int curr=endNode;curr!=-1;curr=parent[curr]){
+        path.push_back(curr);
+    }
+
+    reverse(path.begin(),path.end());
+    return path;
+}
+
+
+
+
+
+void load_roadmap(string filename){
+
+    ifstream file(filename);
+    if(!file.is_open()){
+        cerr<<"could not open file lol "<<filename<<endl;
+        exit(1);
+    }
+
+   
+    string line;
+    while(getline(file,line)){
+
+        if(line.empty()){
+            continue;
+        }
+
+
+        stringstream ss(line);
+        string token;
+        vector<string>tokens;
+
+
+
+        while(getline(ss,token,',')){
+            tokens.push_back(token);
+        }
+        if(tokens.size() < 5){
+            continue;
+        }
+
+
+
+        vector<Node>roadPoints;
+        for(int i=1;i<tokens.size()-2;i+=2){
+            double lon = stod(tokens[i]);
+            double lat = stod(tokens[i+1]);
+            roadPoints.push_back({lon,lat});
+        }
+
+        if (roadPoints.empty()){
+            continue;
+        }
+
+
+        for(int k=0; k<roadPoints.size()-1; k++){
+            Node u = roadPoints[k];
+            Node v = roadPoints[k+1];
+
+            int uID = getID(u);
+            int vID = getID(v);
+            double w = haversine(u,v);
+
+            graph[uID].push_back({vID,w});
+            graph[vID].push_back({uID,w});
+
+            
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+int getNearestNodeID(Node target){
+
+    if(nodeToID.count(target)){
+        return nodeToID[target];
+    }
+
+    int closetsID = -1;
+    double minDist = INF;
+
+    for(int i=0;i<idToNode.size();i++){
+        double dist = haversine(target,idToNode[i]);
+        if(dist<minDist){
+            minDist=dist;
+            closetsID=i;
+        }
+    }
+
+    return closetsID;
+
+}
+
+
+
+
+
+int main(){
+
+    load_roadmap("Roadmap-Dhaka.csv");
+    cout<<"Total distinct intersections: "<<idToNode.size()<<endl;
+
+
+
+
+    Node source,destination;
+    cin>>source.lon>>source.lat;
+    cin>>destination.lon>>destination.lat;
+
+
+    int startID = getNearestNodeID(source);
+    int endID = getNearestNodeID(destination);
+
+  
+    double walkToStart = haversine(source,idToNode[startID]);
+    double walkFromEnd = haversine(idToNode[endID],destination);
+
+
+    vector<int> shortestPath;
+    shortestPath = dijkstra(startID, endID);
+
+
+
+
+    if(shortestPath.empty()){
+        cout << "No path found between these two points " << endl;
+    } 
+    else{
+
+
+        cout << "Shortest path found with " << shortestPath.size() << " steps." << endl;
+        cout << "Coordinates for KML:" << endl;
+        cout << fixed << setprecision(6) << source.lon << "," << source.lat << ",0" << endl;
+        
+
+
+
+
+        //car route
+        for (int id : shortestPath) {
+            Node p = idToNode[id];
+            cout<<fixed<<setprecision(6)<<p.lon<<","<<p.lat <<",0"<<endl;
+        }
+
+        //exact end point (Walking)
+        cout<<fixed<<setprecision(6)<<destination.lon<<","<< destination.lat << ",0" << endl;
+        cout<<"Walking to destination: " <<walkFromEnd<<" km"<<endl;
+    }
+
+    return 0;
+}
